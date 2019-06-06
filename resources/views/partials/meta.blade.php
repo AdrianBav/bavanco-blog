69bdf5ca8620193e7d5f78ca241edc4799020126
Adrian Bavister <adrian@bavanco.co.uk>
2019-06-05 22:21:32 -0500

    Updated twitter meta

--- resources/views/partials/meta.blade.php
+++ resources/views/partials/meta.blade.php
@@ -1,5 +1,7 @@
 <meta name="description" content="{{ $meta['meta_description'] }}">
 
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="@AdrianBavister">
 <meta name="twitter:title" content="{{ $meta['twitter_title'] }}">
 <meta name="twitter:description" content="{{ $meta['twitter_description'] }}">
 <meta name="twitter:image" content="{{ $meta['twitter_image'] }}">
