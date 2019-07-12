00c51cfd6919079d263957782070de287db15403
Adrian Bavister <adrian@bavanco.co.uk>
2019-07-11 22:17:37 -0500

    Added color for mobile bar

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="theme-color" content="#da1e25">
         @if (isset($meta))
             @include('partials.meta')
         @endif
